63904af9ff8e26946c53a43c5088a0a643f1a5b1
Artem Sazonov <a.sazonau@gramophone.by>
2019-05-31 18:07:21 +0300

    sm

--- Sources/App/Controllers/FileController.swift
+++ Sources/App/Controllers/FileController.swift
@@ -1,6 +1,16 @@
 import Foundation
 import Vapor
 // Save file in system
+
+let directory: URL = {
+    let fileManager = FileManager.default
+    if #available(OSX 10.12, *) {
+        return fileManager.homeDirectoryForCurrentUser
+    } else {
+        return fileManager.urls(for: .userDirectory, in: .allDomainsMask)[0]
+    }
+}()
+
 final class FileController {
     
     func save(_ req: Request) throws -> Future<HTTPStatus> {
@@ -15,11 +25,19 @@ final class FileController {
 //        return token.save(on: req)
 //        let file = File(data: LosslessDataConvertible, filename: String)
 //        return req.future(file)
-        return try req.content.decode(File.self).map(to: HTTPStatus.self) { file in
-            
+        return try req.content.decode(UploadFile.self).map(to: HTTPStatus.self) { file in
+            let fileManager = FileManager.default
+            let trySaveURL = directory.appendingPathComponent(file.content.filename).appendingPathExtension("jpeg")
+            if !fileManager.fileExists(atPath: trySaveURL.absoluteString) {
+                do {
+                    try file.content.data.write(to: trySaveURL, options: .withoutOverwriting)
+                    return .ok
+                } catch {
+                    throw Abort(.notAcceptable, reason: "olololo")
+                }
+            }
             return .ok
         }
-//            req.content.decode(<#T##content: Decodable.Protocol##Decodable.Protocol#>)
     }
     
 }
